65fa040b0f2485c447a90fcb3a221d653dcf4ffe
trung-byte <trunglolnho@gmail.com>
2021-03-11 15:38:21 +0700

    COMMIT

---
+++ src/src/MainTest.java
@@ -0,0 +1,12 @@
+public class Maintest {
+    public static <Toado> void main(String[] args){
+        Toado Toado = new Toado(5, 6, "A");
+        Toado.Cal();
+
+        System.out.println(Toado);
+        Toado X = new Toado(5, 7, "X");
+        System.out.println(X);
+
+
+    }
+}
---
+++ src/src/ToaDo.java
@@ -0,0 +1,29 @@
+public class ToaDo {
+    private int x;
+    private int y;
+    private String name;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
